32125eb2b58159fd91a615f637437e69b4255d82
Håkon <144228803+HaakonStokkedal@users.noreply.github.com>
2024-03-07 20:28:33 +0100

    Add project files.

---
+++ Uppgift 6.7/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Uppgift_6._7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ett heltal:");
+            int tal = int.Parse(Console.ReadLine());
+            if (Primtal(tal) == true)
+            {
+                Console.WriteLine($"{tal} är ett primtal.");
+            }
+            else if(Primtal(tal) == false)
+            {
+                Console.WriteLine($"{tal} är inte ett primtal.");
+            }
+            Console.ReadKey();
+        }
+
+        static bool Primtal(int tal)
+        {
+            if (tal < 2)
+            {
+                return true;
+            }
+
+            int namnare = 2;
+
+            while (namnare * namnare <= tal)
+            {
+                if(tal % namnare == 0)
+                {
+                    return false;
+                }
+                namnare++;
+            }
+            return true;
+        }
+    }
+}
